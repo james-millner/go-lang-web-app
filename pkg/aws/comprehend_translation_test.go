6e3090264fb891c606e7a8f2d4ad39690e83e545
James Millner <jamesmillnerdev@gmail.com>
2018-07-13 16:55:59 +0100

    Further tweaks to comprehend testing.

--- pkg/aws/comprehend_translation_test.go
+++ pkg/aws/comprehend_translation_test.go
@@ -11,10 +11,14 @@ func TestTranslate(t *testing.T) {
 
 	entityOneText := "IQBlade"
 	entityTwoText := "Elder"
+	entityThreeText := "Elder"
+	entityFourText := "Elder"
 
 	entities := []comprehend.Entity{
 		comprehend.Entity{Text: &entityOneText, Type: comprehend.EntityTypeOrganization},
 		comprehend.Entity{Text: &entityTwoText, Type: comprehend.EntityTypeOrganization},
+		comprehend.Entity{Text: &entityThreeText, Type: comprehend.EntityTypeOrganization},
+		comprehend.Entity{Text: &entityFourText, Type: comprehend.EntityTypePerson},
 	}
 
 	itemResults := &comprehend.BatchDetectEntitiesItemResult{Entities: entities}
@@ -25,10 +29,9 @@ func TestTranslate(t *testing.T) {
 
 	results := DetermineOrganisationTag(obj)
 
-	assert.Len(t, results, 2)
+	assert.Len(t, results, 3)
 	assert.Equal(t, results[0].Text, "IQBlade")
 	assert.Equal(t, results[0].Tag, "ORGANIZATION")
 	assert.Equal(t, results[1].Text, "Elder")
 	assert.Equal(t, results[1].Tag, "ORGANIZATION")
-
 }
