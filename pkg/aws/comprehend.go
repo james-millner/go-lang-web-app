491e56a098a631402666425bcb52a316b151b02f
James Millner <jamesmillnerdev@gmail.com>
2018-07-13 10:57:30 +0100

    Default config for Comprehend

--- pkg/aws/comprehend.go
+++ pkg/aws/comprehend.go
@@ -3,21 +3,24 @@ package aws
 import (
 	"fmt"
 
-	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/aws/endpoints"
+	"github.com/aws/aws-sdk-go-v2/aws/external"
 	"github.com/aws/aws-sdk-go-v2/service/comprehend"
 )
 
 //RunComprehend
 func RunComprehend(body []string) *comprehend.BatchDetectEntitiesOutput {
 
-	config := aws.Config{
-		Region: "eu-west-1",
+	cfg, err := external.LoadDefaultAWSConfig()
+	if err != nil {
+		panic("unable to load SDK config, " + err.Error())
 	}
+	cfg.Region = endpoints.EuWest1RegionID
 
 	fmt.Println("Config made")
 
 	// Create a Comprehend client from just a session.
-	svc := comprehend.New(config)
+	svc := comprehend.New(cfg)
 
 	fmt.Println("Created client.")
 
